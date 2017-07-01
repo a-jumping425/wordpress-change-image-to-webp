46e78c0e8d76d6f875e5a703e8d23149c6011bf9
Jumping <a.jumping425@gmail.com>
2017-07-02 02:32:31 +0700

    Find img tag from html

--- functions.php
+++ functions.php
@@ -9,7 +9,16 @@ function add_picture_tag_to_content($content) {
     // Check browser
     $user_agent = $_SERVER['HTTP_USER_AGENT'];
     if (strpos($user_agent, 'OPR/') !== false || strpos($user_agent, 'Chrome/') !== false) {    // Chrome or Opera
-    	
+        $upload_dir = wp_upload_dir();
+
+        $html = \simplehtmldom_1_5\str_get_html($content);
+        $img_tags = $html->find('img');
+        foreach($img_tags as $img_tag) {
+
+        }
+
+        $content = $html;
+        return $content;
     }
 
     return $content;
