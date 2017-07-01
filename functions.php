bb62dcbda6c0faafcaec8791a76e02322ca5bb10
Jumping <a.jumping425@gmail.com>
2017-07-02 02:33:21 +0700

    Change img tag as picture tag

--- functions.php
+++ functions.php
@@ -14,7 +14,25 @@ function add_picture_tag_to_content($content) {
         $html = \simplehtmldom_1_5\str_get_html($content);
         $img_tags = $html->find('img');
         foreach($img_tags as $img_tag) {
+            $src = $img_tag->src;
 
+            // Check invalid image url
+            if (strpos($src, $upload_dir['baseurl']) === false)
+                contine;
+
+            // Get image path
+            $img_sub_path = substr($src, strlen($upload_dir['baseurl']));
+            $img_path = $upload_dir['basedir'] . $img_sub_path;
+
+            // Check exist webp
+            if (!file_exists($img_path . '.webp'))
+                continue;
+
+            $webp_url = $upload_dir['baseurl'] . $img_sub_path . '.webp';
+            $img_tag->outertext = '<picture>'
+                . '<source srcset="'. $webp_url .'" type="image/webp">'
+                . $img_tag->outertext
+                . '</picture>';
         }
 
         $content = $html;
