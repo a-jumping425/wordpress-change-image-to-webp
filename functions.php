ec04ce05c8c4b48ab5dd4d09914b311255a5a7dc
Jumping <a.jumping425@gmail.com>
2017-07-02 02:29:53 +0700

    Check browser

--- functions.php
+++ functions.php
@@ -6,5 +6,11 @@ include_once('simple_html_dom.php');
 
 add_filter( 'the_content', 'add_picture_tag_to_content' );
 function add_picture_tag_to_content($content) {
+    // Check browser
+    $user_agent = $_SERVER['HTTP_USER_AGENT'];
+    if (strpos($user_agent, 'OPR/') !== false || strpos($user_agent, 'Chrome/') !== false) {    // Chrome or Opera
+    	
+    }
+
     return $content;
 }
